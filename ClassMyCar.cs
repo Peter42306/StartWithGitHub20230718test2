a38a130997ed86c460654c31faabad083db2aaee
Peter Zalizko <pzalizko@gmail.com>
2023-07-18 23:20:43 +0300

    My first commit: this is my test No. 2 today

---
+++ ClassMyCar.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyProjectToStartGitHubTest2on20230718
+{
+    internal class ClassMyCar
+    {
+    }
+}
